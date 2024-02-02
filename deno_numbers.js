0533878f39741f04d3ef56a9839b75ef3b4e5f6b
lees846 <lees846@newschool.edu>
2024-02-02 14:50:59 -0500

    initial commit, q's 1-3 complete

---
+++ deno_numbers.js
@@ -0,0 +1,32 @@
+/**
+ * This is a test file to get some number and random-based programs going with Deno
+ * Referencing https://www.notion.so/Hello-Deno-827222988a4c45adb6b1e9a8a02004bd
+ * 02 02 2024, Shayla Lee
+
+4. Make a program that asks the user for two numbers and then prints their sum.
+5. Make a program that prints out a random number between 1 and 6 (inclusive)
+6. Make a program that prints either your first name or your last name, chosen at random.
+
+*/
+
+/*
+// 4:
+// Ask for two numbers, and convert them to numbers from strings
+const addend1 = Number(prompt("Give me a number"));
+const addend2 = Number(prompt("Another number"));
+
+// Add them
+const sum = addend1 + addend2;
+
+// Log to console
+console.log(`Did you know those add up to ${sum}?`);
+
+*/
+
+// /*
+// 5:
+const randomNum = Math.random(6);
+// const scaledRandNum = Map(randomNum, 0, 1, 0, 6);
+
+console.log(randomNum);
+// */
---
+++ hello_deno.js
@@ -0,0 +1,4 @@
+// This is a test file to get a "Hello world!" going with Deno
+// 02 02 2024, Shayla Lee
+
+console.log("Hello, Deno!");
