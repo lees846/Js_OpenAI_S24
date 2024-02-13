98b22a92bd2ff7c691c5170852a27953c7ce98d7
lees846 <lees846@newschool.edu>
2024-02-13 00:28:52 -0500

    Add option for who starts sentence, user or AI

--- src/hello_ai/silly_sentence_game.js
+++ src/hello_ai/silly_sentence_game.js
@@ -16,31 +16,54 @@ import { gptPrompt } from "../shared/ai.js";
 main();
 
 async function main() {
-  say("\nLet's play the Silly Sentence Game!");
-
   let sentence = "";
-  const first_word = ask(
-    `We will take turns adding a word to the end of a sentence until one of us ends it with a '.', '!' or '?'. 
-    What should the first word be?\n   `,
-  );
-  sentence = first_word;
-
+  let next_word = "";
   const promptTemplate =
     `We are taking turns adding a word to the end of a sentence until one of us ends it 
     with a '.', '!' or '?'. Only add one word or ending punctuation, and ALWAYS send the existing 
-    portion of the sentence with your new word at the end. Use a variety of adjectives, verbs, and nouns.
+    portion of the sentence with your new word at the end. Use a variety of adjectives, verbs, and 
+    nouns that match the tone of the current sentence.
     Example 1: the current sentence is 'Hello my', you pick 'name', send only 'Hello my name'
     Example 2: current sentence = 'when the world is blue,', send 'when the world is blue, I'
     Example 3: current sentence = 'I wish I could fly, don't you', send 'I wish I could fly, don't you?'.
     Make sure you display the whole sentence and your new word or punctuation at the end.`;
 
-  let next_word = "";
+  say("\nLet's play the Silly Sentence Game!");
+
+  const who_starts = ask(
+    `We will take turns adding a word to the end of a sentence until one of us ends it with a '.', '!' or '?'. 
+  Who should pick the first word, me or you?`,
+  );
+
+  let first_word;
+
+  if (who_starts === "me" || who_starts === "Me" || who_starts === "ME") {
+    first_word = ask(
+      `Great! What's the first word?`,
+    );
+    sentence = first_word;
+  } else {
+    first_word = await gptPrompt(
+      `${promptTemplate} In this case, choose the first word for the sentence. Always send only one word. NEVER send more than one word.
+      Correct Examples: "Good" "When" "Eventually" "Sun"
+      Wrong Examples: "Every day" "Sometime I" "For some"`,
+      { max_tokens: 3, temperature: 1.2 },
+    );
+    sentence = first_word;
+    say(`Let's start with ${sentence}!`);
+    say(`Add the next word:`);
+    next_word = ask(`${sentence} `);
+    sentence += ` ${next_word}`;
+  }
 
   // Referencing https://www.w3schools.com/js/js_string_search.asp#mark_includes for more flexible checking
   while (
     !next_word.includes("!") &&
     !next_word.includes(".") &&
-    !next_word.includes("?")
+    !next_word.includes("?") &&
+    !sentence.includes("!") &&
+    !sentence.includes(".") &&
+    !sentence.includes("?")
   ) {
     sentence = await gptPrompt(
       `${promptTemplate} The current sentence is: '${sentence}'`,
