809ca4e1b12c0a9686cfe3bd2fa0af7113b6a47d
lees846 <lees846@newschool.edu>
2024-02-12 23:41:13 -0500

    Update prompt and adjust temp, etc.

--- src/hello_ai/silly_sentence_game.js
+++ src/hello_ai/silly_sentence_game.js
@@ -16,31 +16,35 @@ import { gptPrompt } from "../shared/ai.js";
 main();
 
 async function main() {
-  say("\nLet's play the silly sentence game!");
+  say("\nLet's play the Silly Sentence Game!");
 
   let sentence = "";
   const first_word = ask(
-    `We will take turns adding a word to the end of a sentence until one of us 
-    ends it with a '.', '!' or '?'. What should the first word be?\n`,
+    `We will take turns adding a word to the end of a sentence until one of us ends it with a '.', '!' or '?'. 
+    What should the first word be?\n   `,
   );
   sentence = first_word;
 
   const promptTemplate =
     `We are taking turns adding a word to the end of a sentence until one of us ends it 
-    with a '.', '!' or '?'. Only add one word or ending punctuation, and always send the existing 
+    with a '.', '!' or '?'. Only add one word or ending punctuation, and ALWAYS send the existing 
     portion of the sentence with your new word at the end. Use a variety of adjectives, verbs, and nouns.
-    Example: the current sentence is 'Hello my', you pick 'name', send only 'Hello my name'.`;
+    Example 1: the current sentence is 'Hello my', you pick 'name', send only 'Hello my name'
+    Example 2: current sentence = 'when the world is blue,', send 'when the world is blue, I'
+    Example 3: current sentence = 'I wish I could fly, don't you', send 'I wish I could fly, don't you?'.
+    Make sure you display the whole sentence and your new word or punctuation at the end.`;
 
   let next_word = "";
 
+  // Referencing https://www.w3schools.com/js/js_string_search.asp#mark_includes for more flexible checking
   while (
-    next_word != "!" &&
-    next_word != "." &&
-    next_word != "?"
+    !next_word.includes("!") &&
+    !next_word.includes(".") &&
+    !next_word.includes("?")
   ) {
     sentence = await gptPrompt(
       `${promptTemplate} The current sentence is: '${sentence}'`,
-      { max_tokens: 50, temperature: 0.7 },
+      { max_tokens: 90, temperature: 1.2 },
     );
 
     say(`Add the next word:`);
@@ -48,5 +52,9 @@ async function main() {
     sentence += ` ${next_word}`;
   }
 
-  say(`/n Our final silly sentence is: \n***\n"${sentence}"\n***\n`);
+  say(
+    `\nOur final silly sentence is: 
+    \n*\n\n"${sentence}"\n\n*\n
+    Thanks for playing!`,
+  );
 }
