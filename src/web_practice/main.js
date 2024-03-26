e7a8e1a1e96f90874cead5fd6ee2b941cf87cbcc
lees846 <lees846@newschool.edu>
2024-03-25 23:32:17 -0400

    nav images toggle which article is shown

--- src/web_practice/main.js
+++ src/web_practice/main.js
@@ -1,1 +1,20 @@
 // This project is made in collaboration with ChatGPT
+
+const navImages = document.getElementsByClassName("nav-image");
+for (let i = 0; i < navImages.length; i++) {
+  navImages[i].addEventListener("click", function (event) {
+    // Refer to the image that was selected
+    const clickedImage = event.currentTarget.querySelector("img");
+
+    // Find the corresponding article
+    const articleId = clickedImage.id.replace("-img", "-article");
+    const targetArticle = document.getElementById(articleId);
+
+    // Check if it has a hidden class, take it away/add it accordingly
+    if (targetArticle.classList.contains("hidden")) {
+      targetArticle.classList.remove("hidden");
+    } else {
+      targetArticle.classList.add("hidden");
+    }
+  });
+}
