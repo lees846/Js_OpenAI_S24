8810bbe3b6bc582a729d580afd268f83efacc4f6
lees846 <lees846@newschool.edu>
2024-04-01 23:44:43 -0400

    initial commit, hello gpt web homework (modify example code)

---
+++ src/hello_web_gpt_mod_040124/hello_web_gpt.js
@@ -0,0 +1,28 @@
+import { Application, Router } from "https://deno.land/x/oak@v12.6.1/mod.ts";
+import { gptPrompt } from "../shared/openai.ts";
+import { createExitSignal, staticServer } from "../shared/server.ts";
+
+import { Chalk } from "npm:chalk@5";
+const chalk = new Chalk({ level: 1 });
+
+const app = new Application();
+const router = new Router();
+
+// API routes
+router.get("/api/gpt", async (ctx) => {
+  // When html script accesses this url, the prompt
+  // is sent to the api with fetch through here
+  const prompt = ctx.request.url.searchParams.get("prompt");
+  const shortPrompt = prompt.slice(0, 128);
+  const result = await gptPrompt(shortPrompt);
+  // Directly add response from api to html as string
+  ctx.response.body = result;
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+app.use(staticServer);
+
+console.log(chalk.green("\nListening on http://localhost:8000"));
+
+await app.listen({ port: 8000, signal: createExitSignal() });
